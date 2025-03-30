ef160a61897b17245e674319f58c97d561ac775d
Reechka <R2echka@gmail.com>
2025-03-30 20:50:51 +0300

    so databases yea

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,32 @@
+package ru.hogwarts.school.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.StudentRepository;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Optional<Student> findStudent(long id) {
+        return studentRepository.findById(id);
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    } 
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+}
